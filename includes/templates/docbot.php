7928f719fa5edcb0ed9f09bc3dae653c5fb6d094
N_Ireland <gfx@minigraphix.ca>
2017-02-15 08:57:10 -0500

    added backfill for the IE8 & 9 classList backcompat issue

--- includes/templates/docbot.php
+++ includes/templates/docbot.php
@@ -1,3 +1,5 @@
+<script src="js/classList.js"></script>
+<!-- the above is backfill for the absence of the classList attribute in IE8 & 9 -->
 <script src="js/main.js"></script>
 <script src="js/pageSwap.js"></script>
 <script src="js/<?php echo $page;?>.js"></script>
