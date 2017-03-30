42d65c1326018c83673451f259ebe4b8d5d14c5c
N_Ireland <gfx@minigraphix.ca>
2017-03-30 17:31:45 -0400

    added video to site homepage / splash page

--- js/home.js
+++ js/home.js
@@ -1,3 +1,81 @@
 (function(){
 
+var vid = document.querySelector('#splashVideo');
+var screenWidth = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth||0;
+function toggleVideo(){
+var toggleSound = document.querySelector('#toggleSound');
+var fade = document.querySelector('#fade');
+
+if(screenWidth>=(16*40)){
+  if(!vid.classList.contains('play')){
+    vid.classList.add('play');
+    vid.style.display = "block";
+    vid.src ="vid/hockey.mp4";
+    vid.play();
+    vid.parentNode.style.background = "#000";
+    console.log("show video");
+    toggleSound.style.display = "block";
+  }
+}else{
+  if(vid.classList.contains('play')){
+    vid.pause();
+    vid.muted = true;
+    fade.style.display = "block";
+    TweenLite.to(fade,0.5,{opacity:1});
+    toggleSound.style.display = "none";
+    toggleSound.classList.toggle('muted');
+    vid.classList.remove('play');
+    vid.style.display = "none";
+    vid.parentNode.style.background = "url('img/ACTION-1.jpg')"
+    vid.parentNode.style.backgroundRepeat = "no-repeat";
+    vid.parentNode.style.backgroundSize= "cover";
+    console.log("hide video");
+  }
+}
+}
+
+
+
+toggleSound.addEventListener('click',function(event){
+  event.preventDefault();
+  toggleSound.classList.toggle('muted');
+  if(toggleSound.classList.contains('muted')){
+    toggleSound.innerHTML = "MUTE";
+    vid.muted = false;
+    TweenLite.to(fade,0.5,{opacity:0,onComplete:function(){
+      fade.style.display = "none";
+    }});
+  }else{
+    vid.muted = true;
+    toggleSound.innerHTML = "UNMUTE";
+    fade.style.display = "block";
+    TweenLite.to(fade,0.5,{opacity:1});
+  }
+},false);
+
+function offsetVideo(){
+  console.log("offsetting video");
+  var vidStyles = window.getComputedStyle(vid);
+  var w = vidStyles.width;
+  if(w==="auto"){
+    w = 1400;
+  }
+  w = parseInt(w);
+  var offset;
+  if(w>screenWidth){
+    offset = (w-screenWidth)/2;
+  }else{
+    offset=0;
+  }
+  // console.log(-offset);
+  TweenLite.to(vid,1,{left:-offset});
+}
+window.addEventListener('resize',function(){
+  screenWidth = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth||0;
+  offsetVideo();
+  toggleVideo();
+},false);
+
+offsetVideo();
+toggleVideo();
 })();
--- pages/home.php
+++ pages/home.php
@@ -1,15 +1,18 @@
 <?php $page = "home" ?>
-<section id="splash" class="dark-overlay">
+<section id="splash" class="">
+  <div id="fade">
   <div class="banner">28,000<div class="back"></div></div>
   <!-- <img src="img/ACTION-1.jpg" alt="background hockey game 1972"> -->
   <div class="callToAction">
     <div class="tagline">It's not what they did. It's how they did it.</div>
     <a href="story" class="ajax white button">learn more</a>
   </div>
-  <!-- <div class="dark-overlay"></div> -->
+  <div class="dark-overlay"></div>
+</div>
     <div class="back-video" id="homepageVideo">
-      <video controls="" width="1920" height="1080" autoplay muted>
-        <source src="vid/hockey.mp4" type="video/mp4">
+      <video id="splashVideo" loop autoplay muted>
+        <source src="#" type="video/mp4">
       </video>
     </div>
+    <a id="toggleSound" class="muted" href="#">UNMUTE</a>
 </section>
