0a70a8590728a3ae33797f93732fae320713c744
N_Ireland <gfx@minigraphix.ca>
2017-04-04 01:24:57 -0400

    work on the history page

--- js/history.js
+++ js/history.js
@@ -1,3 +1,39 @@
 (function(){
+var timeline = document.querySelector('#timeline');
+var topLevelEvents = timeline.querySelectorAll('#events a');
 
+function expandEvent(e){
+  e.preventDefault();
+var it = e.currentTarget;
+var offsetEl = it.parentNode.offsetTop;
+var offsetParent = it.parentNode.parentNode.parentNode.offsetTop;
+var o = offsetEl - offsetParent;
+TweenLite.to(timeline.querySelector('.modal-arrow'),0.5,{top:o-28});
+if(it.classList.contains('tl')){
+  var list = it.parentNode.querySelector('ul');
+  var listLinks = list.parentNode.querySelectorAll('.dropdown a');
+  if(list.classList.contains('open')){
+    var t = listLinks.length;
+    for(l of listLinks){
+      TweenLite.set(l,{opacity:1});
+      TweenLite.to(l,0.5,{opacity:0,delay:t*0.1});
+      t--;
+    }
+    TweenLite.to(list,0.5,{height:0,delay:0.5});
+    }else{
+      TweenLite.set(list,{height:"auto"});
+      TweenLite.from(list,0.5,{height:0});
+      var t = 0;
+      for(l of listLinks){
+        TweenLite.set(l,{opacity:0});
+        TweenLite.to(l,0.5,{opacity:1,delay:t*0.1});
+        t++;
+      }
+    }
+    list.classList.toggle('open');
+  }
+}
+for(link of topLevelEvents){
+  link.addEventListener('click',expandEvent,false);
+}
 })();
--- pages/history.php
+++ pages/history.php
@@ -11,5 +11,39 @@
   <div class="back-img"></div>
 </section>
 <section id="timeline">
-  <div class="placeholder"></div>
+  <h2>TIMELINE</h2>
+  <div class="inner">
+    <div class="timelineCont">
+      <div class="left half">
+        <div class="modal">
+          <div class="modal-arrow"></div>
+          <img src="#" alt="#">
+          <div class="desc">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+      </div>
+      <div class="right half">
+        <ul id="events">
+          <li><a href="#">BEFORE THE SERIES</a></li>
+          <li><a class="tl" href="#">DURING THE SERIES</a>
+              <ul class="dropdown">
+                <li><a href="#">Game One</a></li>
+                <li><a href="#">Game Two</a></li>
+                <li><a href="#">Game Three</a></li>
+                <li><a href="#">Game Four</a></li>
+                <li><a href="#">Game Five</a></li>
+                <li><a href="#">Game Six</a></li>
+                <li><a href="#">Game Seven</a></li>
+                <li><a href="#">Game Eight</a></li>
+              </ul>
+          </li>
+          <li><a href="#">AFTER THE SERIES</a></li>
+        </ul>
+      </div>
+    </div>
+    </div>
+  <div class="dark-overlay darker"></div>
 </section>
