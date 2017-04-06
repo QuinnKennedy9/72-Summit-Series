e6c5a6a6b953905236a53cefc1be6559cd51c7fd
QuinnKennedy9 <qnnkennedy922@gmail.com>
2017-04-06 16:28:56 -0400

    Partnership page
    added the partnership page to the site

--- pages/contact.php
+++ pages/contact.php
@@ -1,5 +1,31 @@
 <?php $page = "contact" ?>
-<p>
-  CONTACT
-  <!-- EDIT THIS FILE FOR CONTACT -->
-</p>
+<section id="splash" class="">
+  <h2 class="hidden">Contact Page</h2>
+  <div id="fade">
+  <div class="banner">Contact Us<div class="back"></div></div>
+  <div class="callToAction">
+    <div class="tagline">Questions, Partnerships, and Events</div>
+  </div>
+  <div class="dark-overlay"></div>
+  <div class="back-img"></div>
+</section>
+<section id="formSection">
+  <form id="form" action="form.php" method="post">
+    <fieldset>
+      <label for="name">Name</label>
+        <input type="text" required id="name" size="30" placeholder="Please Enter Your Name">
+      <label for="email">Email</label>
+        <input type="email" required id="email" size="30" placeholder="Please Enter Your Email">
+      <label for="subject"id="subjectLabel">Subject:</label>
+        <label >Questions</label>
+        <input type="radio" name="subject" size="30">
+        <label >Partnerships</label>
+        <input type="radio" name="subject" size="30">
+        <label >Events</label>
+        <input type="radio" name="subject" size="30">
+        <input type="textarea" required id="message" size="60" placeholder="Enter Your Message">
+        <input type="submit" id="submit" value="Submit">
+    </fieldset>
+  </form>
+
+</section>
--- pages/partnerships.php
+++ pages/partnerships.php
@@ -1,5 +1,30 @@
 <?php $page = "partnerships" ?>
-<p>
-  PARTNERSHIPS
-  <!-- EDIT THIS FILE FOR PARTNERSHIPS -->
-</p>
+<section id="splash" class="">
+  <h2 class="hidden">Partners</h2>
+  <div id="fade">
+  <div class="banner">Our Partners<div class="back"></div></div>
+  <div class="callToAction">
+    <div class="tagline">The 28,800 foundation is possible due to the generous contributions of our partners</div>
+  </div>
+  <div class="dark-overlay"></div>
+  <div class="back-img"></div>
+</section>
+<section id="partners">
+<div id="upperPartner">
+  <h3 class="hidden">Top Row of Partners</h3>
+    <a href="http://www.amusic.mu/" target="_blank"><img src="img/amusic.jpeg" alt="A Music" class="partnerImage"></a>
+    <a href="https://www.aircanada.com" target="_blank"><img src="img/ac.png" alt="Air Canada" class="partnerImage"></a>
+    <a href="http://www.sportshall.ca/" target="_blank"><img src="img/cshof.png" alt="Canada Sports Hall of Fame" class="partnerImage"></a>
+    <a href="https://www.hockeycanada.ca/en-ca" target="_blank"><img src="img/hc_large.png" alt="Hockey Canada" class="partnerImage"></a>
+    <a href="http://arcane.ws/" target="_blank"><img src="img/arcane.png" alt="Arcane" class="partnerImage"></a>
+</div>
+<div id="lowerPartner">
+  <h3 class="hidden">Bottom Row of Partners</h3>
+    <a href="https://brocku.ca/goodman/" target="_blank"><img src="img/brock_large.png" alt="Brock University" class="partnerImage"></a>
+    <a href="http://www.timhortons.com/ca/en/index.php" target="_blank"><img src="img/tim-hortons-logo.png" alt="Time Hortons" class="partnerImage"></a>
+    <a href="https://gibraltarmarket.com/" target="_blank"><img src="img/gibraltar.png" alt="Gibraltar Weekend Market" class="partnerImage"></a>
+    <a href="https://lexcor.ca/" target="_blank"><img src="img/lexcor-logo.png" alt="Lexcor" class="partnerImage"></a>
+</div>
+<a href="contact" id="partnerButton" class="button">Become A Partner</a>
+<div>_</div>
+</section>
