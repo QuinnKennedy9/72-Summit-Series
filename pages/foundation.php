41ecc57010be2823f5d9709a030a39f7e88066a4
QuinnKennedy9 <qnnkennedy922@gmail.com>
2017-04-07 01:56:57 -0400

    change to foundation page

--- pages/foundation.php
+++ pages/foundation.php
@@ -16,12 +16,9 @@
 <div class="back-img"></div>
 </section>
 <section id="portal">
-  <div id="upperPartner">
-    <h3 class="hidden">Top Row of Partners</h3>
-      <a href="http://www.amusic.mu/" target="_blank"><img src="img/amusic.jpeg" alt="A Music" class="partnerImage"></a>
-      <a href="https://www.aircanada.com" target="_blank"><img src="img/ac.png" alt="Air Canada" class="partnerImage"></a>
-      <a href="http://www.sportshall.ca/" target="_blank"><img src="img/cshof.png" alt="Canada Sports Hall of Fame" class="partnerImage"></a>
-      <a href="https://www.hockeycanada.ca/en-ca" target="_blank"><img src="img/hc_large.png" alt="Hockey Canada" class="partnerImage"></a>
-      <a href="http://arcane.ws/" target="_blank"><img src="img/arcane.png" alt="Arcane" class="partnerImage"></a>
+  <div id="intro">The 28-8 foundation is Team Canada's way of giving back to the country that it loves.  The goal of the foundation is to teach the importance of teamwork through the three pillars of Education, Entertainment, and Charity.</div>
+  <div id="tophalf">
+
   </div>
+  <div id="bottomhalf"
 </section
