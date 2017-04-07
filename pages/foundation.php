3c13c8651357300d3e197f0298348864d56dd982
N_Ireland <gfx@minigraphix.ca>
2017-04-07 00:43:03 -0400

    changes to the foundation page, for styling

--- pages/foundation.php
+++ pages/foundation.php
@@ -1,14 +1,20 @@
 <?php $page = "foundation" ?>
 <section id="splash" class="">
   <h2 class="hidden">Foundation Page</h2>
-  <div id="fade">
-  <div class="banner">The 28-8 Foundation<div class="back"></div></div>
+<div id="fade">
+  <div class="banner">
+    <div class="spacer"></div>
+    <div class="back">
+    <div class="fitText">The 28-8 Foundation</div>
+    </div>
   <div class="callToAction">
-    <div class="tagline">Education, Entertainment, Charity</div>
+    <h4 class="subheading">Education, Entertainment, Charity</h4>
+    <a href="#portal" class="scroller white arrow">SCROLL</a>
   </div>
-  <a href="#form" id="formButton" class="scroller white arrow">SCROLL</a>
-  <div class="dark-overlay"></div>
-  <div class="back-img"></div>
+</div>
+  <div class="dark-overlay darker"></div>
+</div>
+<div class="back-img"></div>
 </section>
 <section id="portal">
   <div id="upperPartner">
